b3ff3e9d018e77c107696b6fda07ef4e8f943070
Auven <1273961575@qq.com>
2017-10-27 11:34:29 +0800

    重整目录结构，添加README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+## 基于 Github issues 的单页面静态博客（vue版）
+
+> 本项目基于[github-issues-blog](https://github.com/wuhaoworld/github-issues-blog)的思路，使用vue构建了一个更完善的博客系统。
+
+很多人仍然希望能有一个独立域名、可以自由修改主题的博客。Wordpress 、Typecho 太重，还要买 VPS、部署服务器环境、安装插件、主题，太折腾人，于是我想，完全可以利用 Github 提供的 API 来实现一个只有一个静态页面的博客，具体思路如下：
+
+1. 作者在 Github issues 上写文章（写 issues）
+2. 博客页面通过 JS Ajax 请求 Github API 来获取文章内容，进行页面的渲染
+3. 通过社会化评论插件实现评论功能
+
+## 功能
+
+- [x] 获取 github issues 列表
+- [x] 展示 issues 内容
+- [x] markdown语法编译
+- [x] 代码高亮
+- [ ] 按标签分类博客
+- [ ] 响应式
+- [ ] 评论系统
+- [ ] “不蒜子”访问量统计
+- [ ] 图片预览
+- [ ] 分享
+- [ ] 搜索
+
+## 使用到的框架和插件
+
+- 框架
+  - vue
+- 插件
+  - marked
+  - highlight.js
--- config.js
+++ config.js
@@ -2,9 +2,14 @@ var _config = {
     blog_name       : '用于演示的博客',         // 博客名称
     owner           : 'auven',           // github 用户名
     repo            : 'blog',// github 中对应项目名
-    // duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
     access_token : 'token 19cc7e597a6885' + 'dc6b73740589af41a44744e33a',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
-    per_page        : '15'                    // 默认一页显示几篇文章
+    per_page        : '15',                    // 默认一页显示几篇文章
+    // 版权起始年份
+    since_year: 2015,
+    // icp备案号 ICP_license: 京ICP备1234556号-1
+    ICP_license: false,
+    // 设置为 true 发布后将使用 unpkg cdn
+    cdn: false
 }
 
 // var duoshuoQuery = {short_name:_config['duoshuo_id']};
